4610a0e4fe1433a08c46ba018da13d1f9fa52670
Joshua Needham <joshuabneedham@gmail.com>
2017-10-26 22:07:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
   max_value = 0
-  name_hash.each do |key, value|
-    if name_hash = []
-      return nil
-    else max_value < value
+  if name_hash = ""
+    return nil
+  else name_hash.each do |key, value|
+      max_value < value
       return key
     end
   end
